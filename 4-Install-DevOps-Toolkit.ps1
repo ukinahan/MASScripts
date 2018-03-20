885cc7076061de02d7fd6b6e7ee04951aeebf0e3
Ultan Kinahan <ultanki@outlook.com>
2018-03-20 05:04:11 -0700

    Add files via upload

---
+++ 4-Install-DevOps-Toolkit.ps1
@@ -0,0 +1,12 @@
+﻿    # Variables
+    $Uri = 'https://raw.githubusercontent.com/mattmcspirit/azurestack/master/powershell/DevOpsToolkit.ps1'
+    $LocalPath = 'c:\DevOpsToolkit'
+
+    # Create folder
+    New-Item $LocalPath -Type directory
+
+    # Download file
+    Invoke-WebRequest $uri -OutFile ($LocalPath + '\' + 'DevOpsToolkit.ps1')
+    Set-Location $LocalPath
+
+    .\DevOpsToolkit.ps1 -azureDirectoryTenantName maslab10.onmicrosoft.com -authenticationType AzureAD
