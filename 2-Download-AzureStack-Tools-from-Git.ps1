8a094813d6b7d2365151073a83390e5f084f1b3a
ukinahan <ultanki@outlook.com>
2018-03-20 04:39:09 -0700

    Add files via upload

---
+++ 2-Download-AzureStack-Tools-from-Git.ps1
@@ -0,0 +1,18 @@
+﻿#Install Azure Stack Tools from GitHub
+
+# Change directory to the root directory. 
+cd \
+
+# Download the tools archive.
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12 
+invoke-webrequest `
+  https://github.com/Azure/AzureStack-Tools/archive/master.zip `
+  -OutFile master.zip
+
+# Expand the downloaded files.
+expand-archive master.zip `
+  -DestinationPath . `
+  -Force
+
+# Change to the tools directory.
+cd AzureStack-Tools-master
---
+++ 3-Register-AzureStack.ps1
@@ -0,0 +1,19 @@
+﻿#Register with Azure 
+
+#1 - Run this first, login into Azure with the creds that you want to use to register the stack with in Azure (Azure Credentials)
+Add-AzureRmAccount -EnvironmentName "AzureCloud"
+
+#2 - Paste the subscription GUID ID below
+Get-AzureRmSubscription -SubscriptionID '494b1ae1-78a5-4372-919c-a3d90db0ac1b' | Select-AzureRmSubscription
+
+#3 - Run the remainder of this script
+Register-AzureRmResourceProvider -ProviderNamespace Microsoft.AzureStack
+
+Import-Module .\RegisterWithAzure.psm1
+
+$AzureContext = Get-AzureRmContext
+$CloudAdminCred = Get-Credential -UserName AZURESTACK\CloudAdmin -Message "Enter the cloud domain credentials to access the privileged endpoint"
+Set-AzsRegistration `
+    -CloudAdminCredential $CloudAdminCred `
+    -PrivilegedEndpoint AzS-ERCS01 `
+    -BillingModel Development
